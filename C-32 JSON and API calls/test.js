2b25d2dc9966e6234e53702d4bf848e4642ad889
Eshanvi-Joshi <76736640+Eshanvi-Joshi@users.noreply.github.com>
2021-07-02 17:41:10 +0530

    Add files via upload

---
+++ C-32 JSON and API calls/test.js
@@ -0,0 +1,20 @@
+var name = "Eshanvi";
+console.log(name);
+
+function setup(){
+
+    createCanvas(400,400);
+    console.log(name);
+
+    ////var age = 13;
+    console.log("inside the function setup")
+
+    for(var i = 1; i<4; i++){
+        console.log("loop " +i)
+    }
+}
+
+//console.log(age)
+
+//function to spawn clouds 
+
